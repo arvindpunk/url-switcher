0fa49eca3fea2d111fa624d300a667c45f1ca1a0
Niharika <niharika1397@gmail.com>
2021-10-24 00:09:44 +0530

    added calendar and drive.

--- background.js
+++ background.js
@@ -4,7 +4,22 @@ chrome.runtime.onInstalled.addListener(function() {
             {
                 conditions: [
                     new chrome.declarativeContent.PageStateMatcher({
-                        pageUrl: { hostEquals: 'mail.google.com', schemes: ['https', 'http'] },
+                        pageUrl: { 
+                            urlMatches: 'calendar.google.com',
+                            schemes: ['https', 'http'] 
+                        },
+                    }) , 
+                    new chrome.declarativeContent.PageStateMatcher({
+                        pageUrl: { 
+                            urlMatches: 'drive.google.com',
+                            schemes: ['https', 'http'] 
+                        },
+                    }) , 
+                    new chrome.declarativeContent.PageStateMatcher({
+                        pageUrl: { 
+                            urlMatches: 'mail.google.com',
+                            schemes: ['https', 'http'] 
+                        },
                     })
                 ],
                 actions: [ new chrome.declarativeContent.ShowPageAction() ]
@@ -15,6 +30,15 @@ chrome.runtime.onInstalled.addListener(function() {
 
 chrome.pageAction.onClicked.addListener(function(tab) {
     let url = tab.url
-    url = url.replace(/(?!mail\.google\.com\/mail\/u\/)(\d+)(?=\/)/, "$1" + 1);
+    if (urlMatches = "mail.google.com"){
+        url = url.replace(/(?!mail\.google\.com\/mail\/u\/)(\d+)(?=\/)/, "$1" + 1);
+    }
+    else if (urlMatches = "calendar.google.com"){
+        url = url.replace(/(?!calendar\.google\.com\/calendar\/u\/)(\d+)(?=\/)/, "$1" + 1);
+    }
+    else if (urlMatches = "drive.google.com"){
+        url = url.replace(/(?!drive\.google\.com\/drive\/u\/)(\d+)(?=\/)/, "$1" + 1);
+    }
+    
     chrome.tabs.update(tab.id, { url: url});
 });
